7360475bf4a9356c50cefa756c36e94eade9e3ad
JHua <j_hua@UBUNTU.myguest.virtualbox.org>
2024-02-06 13:11:36 -0500

    added files

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int pp1,pp2,pp3,pp4;
+char p1[30],p2[30],p3[30],p4[30];
+int main(){
+
+    printf("Please enter name for Player1: \n");
+    scanf("%s",p1);
+
+    printf("Please enter name for Player2: \n");
+    scanf("%s",p2);
+
+    printf("Please enter name for Player3: \n");
+    scanf("%s",p3);
+
+    printf("Please enter name for Player4: \n");
+    scanf("%s",p4);
+
+   // printf("%s %s %s %s",p1,p2,p3,p4);   
+}
